2fc3745bb1ccb2abfdeba4619c87f4540f3446af
ᴅαᴍ'ѕ‽ <104802621+damsyx@users.noreply.github.com>
2022-09-18 01:18:45 +0700

    Update info.py

--- Dam/modules/Tools/info.py
+++ Dam/modules/Tools/info.py
@@ -22,88 +22,91 @@ from Dam.utils import extract_user
 
 @Client.on_message(filters.command("id", [".", "-", "^", "!", "?"]) & filters.me)
 async def get_id(client: Client, message: Message):
-         file_id = None
-         user_id = None
-         if message.reply_to_message:
-             rep = message.reply_to_message
+    file_id = None
+    user_id = None
 
-            if rep.audio:
-                file_id = f"**File ID:** `{rep.audio.file_id}`\n"
-                file_id += "**File Type:** `audio`"
-            elif rep.document:
-                file_id = f"**File ID:** `{rep.document.file_id}`\n"
-                file_id += f"**File Type:** `{rep.document.mime_type}`"
-            elif rep.photo:
-                file_id = f"**File ID**: `{rep.photo.file_id}`\n"
-                file_id += "**File Type**: `Photo`"
-            elif rep.sticker:
-                file_id = f"**Sicker ID:** `{rep.sticker.file_id}`\n"
-                if rep.sticker.set_name and rep.sticker.emoji:
-                    file_id += f"**Sticker Set:** `{rep.sticker.set_name}`\n"
-                    file_id += f"**Sticker Emoji:** `{rep.sticker.emoji}`\n"
+    if message.reply_to_message:
+        rep = message.reply_to_message
 
+        if rep.audio:
+            file_id = f"**File ID:** `{rep.audio.file_id}`\n"
+            file_id += "**File Type:** `audio`"
+
+        elif rep.document:
+            file_id = f"**File ID:** `{rep.document.file_id}`\n"
+            file_id += f"**File Type:** `{rep.document.mime_type}`"
+
+        elif rep.photo:
+            file_id = f"**File ID**: `{rep.photo.file_id}`\n"
+            file_id += "**File Type**: `Photo`"
+
+        elif rep.sticker:
+            file_id = f"**Sicker ID:** `{rep.sticker.file_id}`\n"
+            if rep.sticker.set_name and rep.sticker.emoji:
+                file_id += f"**Sticker Set:** `{rep.sticker.set_name}`\n"
+                file_id += f"**Sticker Emoji:** `{rep.sticker.emoji}`\n"
                 if rep.sticker.is_animated:
                     file_id += f"**Animated Sticker:** `{rep.sticker.is_animated}`\n"
                 else:
                     file_id += "**Animated Sticker:** `False`\n"
-        else:
+            else:
                 file_id += "**Sticker Set:** __None__\n"
                 file_id += "**Sticker Emoji:** __None__"
 
-            elif rep.video:
-                file_id = f"**File ID:** `{rep.video.file_id}`\n"
-                file_id += "**File Type:** `Video`"
+        elif rep.video:
+            file_id = f"**File ID:** `{rep.video.file_id}`\n"
+            file_id += "**File Type:** `Video`"
 
-            elif rep.animation:
-                file_id = f"**File ID:** `{rep.animation.file_id}`\n"
-                file_id += "**File Type:** `GIF`"
+        elif rep.animation:
+            file_id = f"**File ID:** `{rep.animation.file_id}`\n"
+            file_id += "**File Type:** `GIF`"
 
-            elif rep.voice:
-                file_id = f"**File ID:** `{rep.voice.file_id}`\n"
-                file_id += "**File Type:** `Voice Note`"
+        elif rep.voice:
+            file_id = f"**File ID:** `{rep.voice.file_id}`\n"
+            file_id += "**File Type:** `Voice Note`"
 
-            elif rep.video_note:
-                file_id = f"**File ID:** `{rep.animation.file_id}`\n"
-                file_id += "**File Type:** `Video Note`"
+        elif rep.video_note:
+            file_id = f"**File ID:** `{rep.animation.file_id}`\n"
+            file_id += "**File Type:** `Video Note`"
 
-            elif rep.location:
-                file_id = "**Location**:\n"
-                file_id += f"  •  **Longitude:** `{rep.location.longitude}`\n"
-                file_id += f"  •  **Latitude:** `{rep.location.latitude}`"
+        elif rep.location:
+            file_id = "**Location**:\n"
+            file_id += f"  •  **Longitude:** `{rep.location.longitude}`\n"
+            file_id += f"  •  **Latitude:** `{rep.location.latitude}`"
 
-            elif rep.venue:
-                file_id = "**Location:**\n"
-                file_id += f"  •  **Longitude:** `{rep.venue.location.longitude}`\n"
-                file_id += f"  •  **Latitude:** `{rep.venue.location.latitude}`\n\n"
-                file_id += "**Address:**\n"
-                file_id += f"  •  **Title:** `{rep.venue.title}`\n"
-                file_id += f"  •  **Detailed:** `{rep.venue.address}`\n\n"
+        elif rep.venue:
+            file_id = "**Location:**\n"
+            file_id += f"  •  **Longitude:** `{rep.venue.location.longitude}`\n"
+            file_id += f"  •  **Latitude:** `{rep.venue.location.latitude}`\n\n"
+            file_id += "**Address:**\n"
+            file_id += f"  •  **Title:** `{rep.venue.title}`\n"
+            file_id += f"  •  **Detailed:** `{rep.venue.address}`\n\n"
 
-            elif rep.from_user:
-                user_id = rep.from_user.id
+        elif rep.from_user:
+            user_id = rep.from_user.id
 
-         if user_id:
-            if rep.forward_from:
-                user_detail = f"👀 **ID Pengguna Yang Diteruskan:** `{message.reply_to_message.forward_from.id}`\n"
-            else:
-                user_detail = (
-                    f"🙋‍♂️ **ID Pengguna:** `{message.reply_to_message.from_user.id}`\n"
-                )
-            user_detail += f"💬 **ID Pesan:** `{message.reply_to_message.message_id}`"
-            await message.reply(user_detail)
-        elif file_id:
-            if rep.forward_from:
-                user_detail = f"👀 **ID Pengguna Yang Diteruskan:** `{message.reply_to_message.forward_from.id}`\n"
-            else:
-                user_detail = (
-                    f"🙋‍♂️ **ID Pengguna:** `{message.reply_to_message.from_user.id}`\n"
-                )
-            user_detail += f"💬 **ID Pesan:** `{message.reply_to_message.message_id}`\n\n"
-            user_detail += file_id
-            await message.reply(user_detail)
-
+    if user_id:
+        if rep.forward_from:
+            user_detail = f"👀 **ID Pengguna Yang Diteruskan:** `{message.reply_to_message.forward_from.id}`\n"
         else:
-            await message.reply(f"👥 **ID Obrolan:** `{message.chat.id}`")
+            user_detail = (
+                f"🙋‍♂️ **ID Pengguna:** `{message.reply_to_message.from_user.id}`\n"
+            )
+        user_detail += f"💬 **ID Pesan:** `{message.reply_to_message.message_id}`"
+        await message.reply(user_detail)
+    elif file_id:
+        if rep.forward_from:
+            user_detail = f"👀 **ID Pengguna Yang Diteruskan:** `{message.reply_to_message.forward_from.id}`\n"
+        else:
+            user_detail = (
+                f"🙋‍♂️ **ID Pengguna:** `{message.reply_to_message.from_user.id}`\n"
+            )
+        user_detail += f"💬 **ID Pesan:** `{message.reply_to_message.message_id}`\n\n"
+        user_detail += file_id
+        await message.reply(user_detail)
+
+    else:
+        await message.reply(f"👥 **ID Obrolan:** `{message.chat.id}`")
 
 
 # info user variable
