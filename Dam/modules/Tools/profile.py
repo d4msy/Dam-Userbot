74c7dd49fd25c8f3db77a73ca3173e68223c273a
ᴅαᴍ'ѕ‽ <104802621+damsyx@users.noreply.github.com>
2022-09-18 17:23:08 +0700

    Update profile.py

--- Dam/modules/Tools/profile.py
+++ Dam/modules/Tools/profile.py
@@ -15,67 +15,67 @@ profile_photo = "ProjectMan/modules/cache/pfp.jpg"
 @Client.on_message(filters.command(["block"], [".", "-", "^", "!", "?"]) & filters.me)
 async def block_user_func(client: Client, message: Message):
     user_id = await extract_user(message)
-    Man = await message.reply("💈 `Processing...`")
+    Man = await message.reply("♻️ `Processing...`")
     if not user_id:
         return await message.edit(
-            "Berikan User ID/Username atau reply pesan pengguna untuk membuka blokir."
+            "Provide User ID/Username or reply to user messages to blocking."
         )
     if user_id == client.me.id:
         return await Man.edit("anda stress harap segera minum obat.")
     await client.block_user(user_id)
     umention = (await client.get_users(user_id)).mention
-    await message.edit(f"**Berhasil Memblokir** {umention}")
+    await message.edit(f"**Successfully Blocking** {umention}")
 
 
 @Client.on_message(filters.command(["unblock"], [".", "-", "^", "!", "?"]) & filters.me)
 async def unblock_user_func(client: Client, message: Message):
     user_id = await extract_user(message)
-    Man = await message.reply("💈 `Processing...`")
+    Man = await message.reply("♻️ `Processing...`")
     if not user_id:
         return await message.edit(
-            "Berikan User ID/Username atau reply pesan pengguna untuk membuka blokir."
+            "Provide User ID/Username or reply to user messages to unblock."
         )
     if user_id == client.me.id:
         return await Man.edit("anda stress harap segera minum obat.")
     await client.unblock_user(user_id)
     umention = (await client.get_users(user_id)).mention
-    await message.edit(f"**Berhasil Membuka Blokir** {umention}")
+    await message.edit(f"**Successfully Unblock** {umention}")
 
 
 @Client.on_message(filters.command(["setname"], [".", "-", "^", "!", "?"]) & filters.me)
 async def setname(client: Client, message: Message):
-    Man = await message.reply("💈 `Processing...`")
+    Man = await message.reply("♻️ `Processing...`")
     if len(message.command) == 1:
         return await Man.edit(
-            "Berikan teks untuk ditetapkan sebagai nama telegram anda."
+            "Provide text to set as your telegram name."
         )
     elif len(message.command) > 1:
         name = message.text.split(None, 1)[1]
         try:
             await client.update_profile(first_name=name)
-            await Man.edit(f"**Berhasil Mengubah Nama Telegram anda Menjadi** `{name}`")
+            await Man.edit(f"**Successfully Changed Your Telegram Name To** `{name}`")
         except Exception as e:
             await Man.edit(f"**ERROR:** `{e}`")
     else:
         return await Man.edit(
-            "Berikan teks untuk ditetapkan sebagai nama telegram anda."
+            "Provide text to set as your telegram name."
         )
 
 
 @Client.on_message(filters.command(["setbio"], [".", "-", "^", "!", "?"]) & filters.me)
 async def set_bio(client: Client, message: Message):
-    Man = await message.reply("💈 `Processing...`")
+    Man = await message.reply("♻️ `Processing...`")
     if len(message.command) == 1:
-        return await Man.edit("Berikan teks untuk ditetapkan sebagai bio.")
+        return await Man.edit("Provide text to set as bio.")
     elif len(message.command) > 1:
         bio = message.text.split(None, 1)[1]
         try:
             await client.update_profile(bio=bio)
-            await Man.edit(f"**Berhasil Mengubah BIO anda menjadi** `{bio}`")
+            await Man.edit(f"**Successfully Changed your BIO to** `{bio}`")
         except Exception as e:
             await Man.edit(f"**ERROR:** `{e}`")
     else:
-        return await Man.edit("Berikan teks untuk ditetapkan sebagai bio.")
+        return await Man.edit("Provide text to set as bio.")
 
 
 @Client.on_message(filters.me & filters.command(["setpfp"], [".", "-", "^", "!", "?"]))
@@ -93,10 +93,10 @@ async def set_pfp(client: Client, message: Message):
         await client.set_profile_photo(profile_photo)
         if os.path.exists(profile_photo):
             os.remove(profile_photo)
-        await message.edit("**Foto Profil anda Berhasil Diubah.**")
+        await message.edit("**Your Profile Photo Has Been Successfully Changed.**")
     else:
         await message.edit(
-            "`Balas ke foto apa pun untuk dipasang sebagai foto profile`"
+            "`Reply to any photo to set as profile photo.`"
         )
         await sleep(3)
         await message.delete()
@@ -110,7 +110,7 @@ async def view_pfp(client: Client, message: Message):
     else:
         user = await client.get_me()
     if not user.photo:
-        await message.edit("Foto profil tidak ditemukan!")
+        await message.edit("Profile photo not found!")
         return
     await client.download_media(user.photo.big_file_id, file_name=profile_photo)
     await client.send_photo(
